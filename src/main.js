035594ffa959a47d163c74dedc1eb966f7b7139d
unocongafas <unocongafas@gmail.com>
2020-10-23 15:52:07 +0000

    Install router

--- src/main.js
+++ src/main.js
@@ -1,12 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 import "bootstrap";
 import "./style.css"
 
-import Home from "./components/home.js"
+import Footer from "./components/footer.js"
+import Navbar from "./components/navbar.js"
+
+import Home from "./pages/home.js"
+import About from "./pages/about.js"
+import Contact from "./pages/contact.js"
 
 const container = document.querySelector("#app");
-const component = <Home />;
+const component = (
+    <Router>
+        <Navbar />
+        
+        <div className="page">
+            <Switch>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+
+                <Route path="/about">
+                    <About />
+                </Route>
+
+                <Route path="/contact">
+                    <Contact />
+                </Route>
+            </Switch>
+        </div>
+
+        <Footer />
+    </Router>
+);
 
 ReactDOM.render(component, container);
