36f85b86b383ba9faec6662169bae5120f9e0e39
unocongafas <unocongafas@gmail.com>
2020-10-23 15:51:29 +0000

    Create pages

---
+++ src/pages/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function(props) {
+    return (
+        <div className="container">
+            <div className="jumbotron">
+                <h1>Home page</h1>
+            </div>
+        </div>
+    )
+}
